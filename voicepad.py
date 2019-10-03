c225826eaee59076295dca98cd15d1e830c21dad
tuxfoo <tuxfoo@librem.one>
2019-10-03 14:12:21 +1300

    Fixed issue where the last word in pattern always triggers if spoken

--- voicepad.py
+++ voicepad.py
@@ -3,7 +3,9 @@ import playsound as snd
 import json
 import random
 import keyboard
+import mouse
 import time
+import threading
 
 with open('kate.json') as file:
     data = json.load(file)
@@ -12,6 +14,8 @@ sounds = '/home/jacob/Music/Sounds/hcspack-KATE/'
 r = sr.Recognizer()
 phrase = ""
 disabled_categories = []
+# Set to one to enable the online speech api
+online_api = 1
 
 
 def respond(words):
@@ -21,8 +25,15 @@ def respond(words):
         phrase = ""
         if not is_disabled(trigger["Categories"]):
             if check_pattern(words, trigger['patterns']) and phrase.lower() in words and phrase != "":
-                print("Event Triggered: " + trigger['Name'])
-                run_cmd(trigger['Command'])
+                print("Event Triggered: " + trigger['Name'] + " Matched Phrase: " + phrase)
+                if type(trigger['Command']) == list:
+                    # Maybe use threading to run this in background?
+                    # Maybe make running in background optional via json config?
+                    if trigger['Command'][0] != "background=False":
+                        cmd_thread = threading.Thread(target=run_cmd, args=[trigger['Command']])
+                        cmd_thread.start()
+                    else:
+                        run_cmd(trigger['Command'])
                 if trigger['Files'] != "none":
                     snd.playsound(sounds + random.choice(trigger['Files']))
 
@@ -31,9 +42,11 @@ def check_pattern(words, patterns):
     global phrase
     hit = False
     for pattern in patterns:
+        print("Last Phrase: " + phrase)
         if type(pattern) == list:
             if not check_pattern(words, pattern):
                 return False
+            hit = True
         elif phrase == "":
             if contains_phrase(words, pattern.lower()):
                 hit = True
@@ -41,7 +54,7 @@ def check_pattern(words, patterns):
         elif contains_phrase(words, (phrase + " " + pattern).lower()):
             hit = True
             phrase += " " + pattern.lower()
-        elif pattern == patterns[-1] and not hit:
+        if pattern == patterns[-1] and not hit:
             return False
     return True
 
@@ -78,27 +91,30 @@ def is_disabled(cat):
 def run_cmd(commands):
     global sounds
     # This function will take a list of commands and tasks to execute
-    # It does not do anything at the moment, it is here for planning features.
-    if type(commands) != list and commands != "none":
-        return
+    # It does not do much at the moment.
+    # Maybe use threading to do this in background?
     for cmd in commands:
         # The commands will be performed in this loop
         cmd2 = cmd.split("=")
         print(cmd2)
         if cmd2[0] == "wait":
-            # Will take a interger and wait that amount of time
+            # Will take a integer and wait that amount of time
             time.sleep(int(cmd2[1]))
-        elif cmd2[0] == "keybind":
-            # Will take a string, which will be entered on a virtual keyboard
+        elif cmd2[0] == "keypress_release":
+            # Will take a key, which will be pressed and released
             keyboard.press_and_release(cmd2[1])
         elif cmd2[0] == "play":
-            # Will take a file, and play it
+            # Will take a sound file, and play it
             snd.playsound(sounds + cmd2[1])
         elif cmd2[0] == "disable-cat":
             # Will disable category.
             disable_cat(cmd2[1])
         elif cmd2[0] == "enable-cat":
+            # Enables a category
             enable_cat(cmd2[1])
+        elif cmd2[0] == "mouse_click":
+            # Clicks a given mouse button
+            mouse.click(button=cmd2[1])
 
 
 while True:
@@ -107,11 +123,22 @@ while True:
         r.adjust_for_ambient_noise(source, duration=1)
         answer = r.listen(source, phrase_time_limit=4)
         try:
-            ai = r.recognize_wit(answer, key="AJFK24SWSLG2CSKDB2AYRJQWIM47HVFB")
+            if online_api == 1:
+                ai = r.recognize_wit(answer, key="AJFK24SWSLG2CSKDB2AYRJQWIM47HVFB")
             ans = r.recognize_sphinx(answer)
-            print(ans)
-            print(ai)
-            respond(ai.lower())
+            if ai != "":
+                print("-----YOU SAID------")
+                print("pocketphinx: " + ans)
+                if online_api == 1:
+                    print("wit: " + ai)
+                print("-------------------")
+                start = time.time()
+                if online_api == 1:
+                    respond(ai.lower())
+                else:
+                    respond(ans.lower())
+                end = time.time()
+                print("Respond took: " + str(end - start) + " seconds")
             phrase = ""
         except sr.UnknownValueError:
             print("Could not understand audio")
